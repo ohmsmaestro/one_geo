faf19fd50cef5808cb288c29b03c5e8df9478a6f
OhmsDev <muhammed.omenesa@microbridge.com.ng>
2022-01-24 23:45:35 +0100

    base url changed to http://185.4.176.148 from http://69.64.72.176

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
 let BASE_URL = `https://onegeolims.herokuapp.com`;
-BASE_URL = `http://69.64.72.176`;
+//BASE_URL = `http://69.64.72.176`;
+BASE_URL = 'http://185.4.176.148';
 let SERVER = `:7878/api`;
 let MAP_URL = `http://69.64.72.176:9099`;
 
