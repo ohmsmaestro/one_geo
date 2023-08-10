a3b5244306fa5fbdf7970e0776cecd9355be8d06
OhmsDev <muhammed.omenesa@microbridge.com.ng>
2023-08-10 07:50:42 +0100

    jamilu begin

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,6 +2,7 @@ let BASE_URL = `https://onegeolims.herokuapp.com`;
 BASE_URL = `http://185.4.176.77`; // Dev Account
 // BASE_URL = "http://185.4.176.166"; // Live Account
 // BASE_URL = "https://e85a-102-89-22-197.ngrok-free.app"; // Local Account
+BASE_URL = `http://localhost:7878`; // Dev Account
 
 let SERVER = `/api`;
 //let MAP_URL = `http://69.64.72.176:9099`;
